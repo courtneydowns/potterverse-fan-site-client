c334e933180b3e2ba20c23a946d56ee95e96fb1f
courtneydowns <84096318+courtneydowns@users.noreply.github.com>
2021-11-13 14:59:10 -0500

    heroku push

--- src/helper/environment.js
+++ src/helper/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
   case "127.0.0.1":
     APIURL = "http://localhost:3000";
     break;
-  case "potterverse-fan-site-server.herokuapp.com":
+  case "potterverse-fan-site.herokuapp.com":
     APIURL = "https://potterverse-fan-site-server.herokuapp.com";
 }
 
